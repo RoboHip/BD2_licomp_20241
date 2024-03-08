7695bf5aa13b141888d9f8426510a5da6658c30a
RoboHip <thiagoladislausantos@gmail.com>
2024-03-07 21:14:38 -0300

    comandos de manipulação de banco de dados

---
+++ comandoDDL.sql
@@ -0,0 +1,15 @@
+-- acessando o mysql pelo terminal do xampp
+mysql -u root
+
+-- visualiando os bancos de dados instalados 
+show databases; 
+
+-- criando um novo banco de dados
+create database exemplodb; 
+
+-- acessando o banco de dados
+use exemplodb;
+
+-- excluindo um banco de dados
+drop database exemplodb;
+
