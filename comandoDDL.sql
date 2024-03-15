f6266d969b1ed5e72f59e3466f564978a6f9670f
RoboHip <thiagoladislausantos@gmail.com>
2024-03-14 21:05:38 -0300

    aula do dia 14/03

--- comandoDDL.sql
+++ comandoDDL.sql
@@ -5,11 +5,47 @@ mysql -u root
 show databases; 
 
 -- criando um novo banco de dados
-create database exemplodb; 
+create database exemploDB; 
 
 -- acessando o banco de dados
-use exemplodb;
+use exemploDB;
 
 -- excluindo um banco de dados
-drop database exemplodb;
+drop database exemploDB;
+
+-- criar o bancoDB
+create database bancoDB;
+
+-- acessar o bancoDB
+use bancoDB;
+
+-- criando a tabela bancos
+create table bancos(
+    numero int not null, 
+    nome varchar (100) not null, 
+    constraint bancos_pk primary key (numero)
+);
+
+-- criar a tabela agencias
+create table agencias(
+    numero varchar(100) not null,
+    nome varchar (100) not null,
+    telefone int,
+    banco int not null,
+    constraint agencias_pk
+    primary key (numero)
+);
+
+-- criar a tabela clientes 
+create table clientes(
+    id int not null auto_increment,
+    nome varchar (100) not null,
+    email varchar (100) not null,
+    constraint cliente_pk
+    primary key (id)
+);
+
+-- criar a coluna uf do tipo char com tamanho 2 e valor padrão 'RJ' na tabela clientes
+alter table clientes
+add uf char(2) default 'RJ';
 
